2fe73aa82b101802b8d113decebfabc68a30b0f8
Inayath <inayathh2@gmail.com>
2024-04-22 00:53:19 +0530

    Refactor: get all stories service
    switched from using aggregate pipeline to individual finds to limit the number of documents being processed and thus limiting db computation.

--- apps/server/src/controllers/story/getStories.ts
+++ apps/server/src/controllers/story/getStories.ts
@@ -2,7 +2,11 @@ import { RequestHandler } from "express";
 import { storyService } from "../../services/story";
 
 export const getStoriesController: RequestHandler = async (req, res, next) => {
-    const data = await storyService.getAllStories()
+    let { limit: limitQuery } = req.query;
+
+    const limit = isFinite(limitQuery as unknown as number) ? Number(limitQuery) : 0;
+
+    const data = await storyService.getAllStories(limit)
 
     return res.status(200).json(data)
 }
--- apps/server/src/services/story.ts
+++ apps/server/src/services/story.ts
@@ -1,13 +1,10 @@
 import { IAddStoryBody } from "../middlewares/story/validateAddStoryBody";
 import { IStory, Story } from "../models/story";
+import { categoryService } from "./category";
 
 
+type IStoryData = Pick<IStory, "category" | "createdAt" | "slides">
 
-interface IGetAllStoriesResult {
-    data: {
-        [k: string]: Pick<IStory, "category" | "createdAt" | "slides">[]
-    }
-}
 
 
 
@@ -19,49 +16,21 @@ class StoryService {
     }
 
 
-    // async getAllStories() {
-    //     const data = await Story.aggregate([
-    //         { $match: { _id: { $exists: true } } },
-    //         { $project: { updatedAt: 0, created_by: 0 } },
-    //         { $group: { _id: "$category", stories: { $push: "$$ROOT" }, } }
-    //         // { $project: { data: { $arrayToObject: "$$ROOT" } } }
-    //     ])
 
-    //     const result: Record<string, any> = {};
+    async getAllStories(limit: number) {
+        const data: Record<string, IStoryData[]> = {};
 
-    //     data.forEach(d => {
-    //         result[d._id] = d.stories
-    //     })
+        // get all categories
+        const { categories } = await categoryService.getCategoryNames();
 
-    //     return result
-    // }
+        // get documents of each category
+        await Promise.all(categories.map(async (c) => {
+            const result = await Story.find({ category: c }, { updatedAt: 0, created_by: 0 }, { limit }).exec()
+            data[c] = result
+        }))
 
 
-
-    async getAllStories() {
-        const result = await Story.aggregate<IGetAllStoriesResult>([
-            // match all documents
-            { $match: { _id: { $exists: true } } },
-
-            // exclude updatedAt and created_by fields
-            { $project: { updatedAt: 0, created_by: 0 } },
-
-            // group documents by category
-            { $group: { _id: "$category", stories: { $push: "$$ROOT" }, } },
-
-            // rename _id and stories field to k and v
-            { $addFields: { k: "$_id", v: "$stories" } },
-            { $project: { _id: 0, stories: 0 } },
-
-            // push all documents into a field called data
-            { $group: { _id: null, data: { $push: "$$ROOT" } } },
-
-            // convert array of documents containing k(category) and v(array of documents whose category is "k") into a document with k as fieldName and v as value
-            { $project: { data: { $arrayToObject: "$data" }, _id: 0 } }
-        ])
-
-
-        return result[0]
+        return data
     }
 
 }
