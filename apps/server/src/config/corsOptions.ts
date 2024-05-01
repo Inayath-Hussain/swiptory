a4c00aa0b50e4f54e9516333bdcd677e858fd4c5
Inayath <inayathh2@gmail.com>
2024-05-02 03:09:19 +0530

    Fix: vercl config for server

--- apps/server/src/config/corsOptions.ts
+++ apps/server/src/config/corsOptions.ts
@@ -4,7 +4,7 @@ import { env } from "./env"
 export const corsOptions: CorsOptions = env.isProd ?
     {
         // add react deployed origin here.
-        origin: ["*"],
+        origin: ["https://swiptory-ashy.vercel.app/"],
         credentials: true
     }
     :
